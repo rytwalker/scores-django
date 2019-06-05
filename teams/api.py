4d9ab3ac054d8b388ef35164aa2f9a2c2e5d1559
Ryan Walker <rytwalker@gmail.com>
2019-06-05 18:48:57 -0400

    added some bad ideas

--- teams/api.py
+++ teams/api.py
@@ -1,3 +1,5 @@
+import io
+from rest_framework.parsers import JSONParser
 from rest_framework import serializers, viewsets
 from .models import Team
 from scores.api import ScoreSerializer
@@ -6,10 +8,15 @@ from scores.api import ScoreSerializer
 class TeamSerializer(serializers.ModelSerializer):
     # scores = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     scores = ScoreSerializer(many=True, read_only=True)
+    average_score = serializers.SerializerMethodField()
 
     class Meta:
         model = Team
-        fields = ('id', 'team_name', 'scores')
+        fields = ('id', 'team_name', 'scores', 'average_score')
+
+    def get_average_score(self, object):
+
+        return object.team_name
 
 
 class TeamViewSet(viewsets.ModelViewSet):
